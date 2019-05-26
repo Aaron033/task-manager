ad07b84bee2cb8a44392f79c915ef216fe5f8b07
Aaron <aaron.botello.h@gmail.com>
2019-05-26 10:55:22 -0500

    findOne function commented out

--- mongodb.js
+++ mongodb.js
@@ -16,15 +16,15 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 //The user is just a function argument
 
 // This code needs a ObjectID functionality 
-    db.collection('users').findOne({_id: new ObjectID( "5ce9663b8fe4e5adf5cc323f")}, (error, user) =>{ 
-        if(error){
-            return console.log('Unable to fetch')
-        }
-        console.log(user)
-    } )
+    // db.collection('users').findOne({_id: new ObjectID( "5ce9663b8fe4e5adf5cc323f")}, (error, user) =>{ 
+    //     if(error){
+    //         return console.log('Unable to fetch')
+    //     }
+    //     console.log(user)
+    // } )
     //toArray is the callback and it is founded in collection/find/return(cursor)
     //Cursor is a pointer to data and has too many method and one of them is toArray 
-db.collection('users').find({age:25}).toArray((error, users) => {
+db.collection('users').find({age: 25}).toArray((error, users) => {
     console.log(users)
 })
 
