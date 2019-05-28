cde8126aa4fd8f79cb47fd89e31b7315046bfa95
Aaron <aaron.botello.h@gmail.com>
2019-05-27 20:26:53 -0500

    creating instances of User

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,4 +8,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useCreateIndex: true
 })
 
-const User = mongoose.model('User', )
+const User = mongoose.model('User', {
+    name: {
+    type: String
+
+    }, 
+
+    age: {
+    type: Number
+
+    }
+} )
+
+//We are creating instances of the model User 
+const me = new User
