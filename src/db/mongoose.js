cd88317c526ff5e6b6638a6c977fb9c62a973593
Aaron <aaron.botello.h@gmail.com>
2019-05-27 20:17:52 -0500

    connecting mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect
+//Mongose data is going to look different 
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+
+  useNewUrlParser:true, 
+  //
+  useCreateIndex: true
+})
