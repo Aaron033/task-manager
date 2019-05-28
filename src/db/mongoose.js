18b2a24877fbfa336c4b0ee31ee84c7a242596b7
Aaron <aaron.botello.h@gmail.com>
2019-05-27 20:23:40 -0500

    getting the connection to user documents

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 //Mongose data is going to look different 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
-  useNewUrlParser:true, 
-  //
+  useNewUrlParser: true, 
+  //It allows mongoose to  working with mongodb to create indexes and access the data we need to access 
   useCreateIndex: true
 })
+
+const User = mongoose.model('User', )
