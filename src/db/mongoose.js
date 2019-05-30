08b9b50a193d3c30e10f972f0f8a601ffed925c3
Aaron <aaron.botello.h@gmail.com>
2019-05-29 20:28:00 -0500

    adding validator npm

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+// Validator to check emails, credit cards, etc. 
+
 
 //Mongose data is going to look different 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
