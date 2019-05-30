8355afc7ebead7c141a9493ab77bfb972bca6a3d
Aaron <aaron.botello.h@gmail.com>
2019-05-29 19:44:46 -0500

    constructor function created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -20,5 +20,10 @@ const User = mongoose.model('User', {
     }
 } )
 
-//We are creating instances of the model User 
-const me = new User
+//We are creating instances of the model User  constructor function 
+const me = new User ({
+    name: 'Aaron', 
+    age: 25
+})
+
+me.save()
