c0aaa840eefc8588abcdd66fc45a7d9bc0b0e23a
Aaron <aaron.botello.h@gmail.com>
2019-06-01 11:39:45 -0500

    installing express and nodemon

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,7 @@ const validator = require('validator')
 // Validator to check emails, credit cards, etc. 
 
 
+
 //Mongoose collection is written different from mongodb 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
