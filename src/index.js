9b345d667108fb02beeada12b8430ccc7794ec72
Aaron <aaron.botello.h@gmail.com>
2019-06-01 16:43:30 -0500

    mongose require to make new instances

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const express = require('express')
+require('./db/mongoose')
+//It is not going to grab anything from the file; it just going to ensure 
+//That mongoose connects to the data base
+const User = require('./models/user')
+
 
 const app = express()
 //The code below is needed in order to develop the app in heroku 
@@ -8,8 +13,8 @@ app.use(express.json())
 
 //in order to access a http page we must use an http functionality 
 app.post('/users', (req, res) => { 
-    console.log(req.body)
-    res.send('Testing!')
+    // now we can create an instance of user
+   const user = new User(req.body)
 })
 //The first argument is the path and the second is the callback
 
