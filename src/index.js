db200297b026afe6c33f97584e2437846421f860
Aaron <aaron.botello.h@gmail.com>
2019-06-01 11:56:41 -0500

    setting the start up in package.json

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ const app = express()
 //The code below is needed in order to develop the app in heroku 
 const port = process.env.PORT || 3000 
 
-app.listen
+app.listen(port, () => { 
+    console.log('Server is up on port ' + port)
+})
