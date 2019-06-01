82925a74eb6399bf55e4212877b1c9557861ab64
Aaron <aaron.botello.h@gmail.com>
2019-05-31 22:10:08 -0500

    task completed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -62,27 +62,41 @@ const User = mongoose.model('User', {
 
 //We define the second model that will appear in the collection section 
 const Task = mongoose.model('Task', {
-    taskName:{
-        type: String
+    description:{
+        type: String,
+        trim: true, 
+        required: true,
+        
     }, 
     completed: {
-        type: Boolean
+        type: Boolean, 
+        default: false
     }
 })
 
-const me = new User ({
-    name: 'Aaron', 
-    email:'hello@houston.com'
-    age: 25, 
-    password: 'cat525200!'
+// const me = new User ({
+//     name: 'Aaron', 
+//     email:'hello@houston.com',
+//     age: 25, 
+//     password: 'cat525200!'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+
+
+const newTask = new Task ({
+    description : "Do lanudry", 
+    completed: true
 })
 
-me.save().then(() => {
-    console.log(me)
-
-}).catch((error) => {
-    console.log('Error!', error)
-})
-
-
-
+newTask.save().then(() => {
+    console.log(newTask)
+}).catch((error) =>{ 
+    console.log('Error', error)
+}) 
