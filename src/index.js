c0fa9540b5ad316c3c8d92a2bff15d2d711619eb
Aaron <aaron.botello.h@gmail.com>
2019-06-01 12:02:19 -0500

    using a http request

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const app = express()
 //The code below is needed in order to develop the app in heroku 
 const port = process.env.PORT || 3000 
 
+//in order to access a http page we must use an http functionality 
+app.post('/users', (req, res) => { 
+    res.send('Testing!')
+})
+//The first argument is the path and the second is the callback
+
+
 app.listen(port, () => { 
     console.log('Server is up on port ' + port)
 })
