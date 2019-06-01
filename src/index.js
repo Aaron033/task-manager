5c372cea8c1baac2e7a618d4b157e059db04171a
Aaron <aaron.botello.h@gmail.com>
2019-06-01 16:11:09 -0500

    adding express json() to get an object response

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ const app = express()
 //The code below is needed in order to develop the app in heroku 
 const port = process.env.PORT || 3000 
 
+app.use(express.json())
+
 //in order to access a http page we must use an http functionality 
 app.post('/users', (req, res) => { 
+    console.log(req.body)
     res.send('Testing!')
 })
 //The first argument is the path and the second is the callback
