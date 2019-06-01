e7f805477e0d64c35c49b432058f4af509056c41
Aaron <aaron.botello.h@gmail.com>
2019-06-01 16:14:36 -0500

    migrating user info to a new file

---
+++ src/models/user.js
@@ -0,0 +1,47 @@
+
+const User = mongoose.model('User', {
+    name: {
+    type: String,
+    require: true, 
+    trim: true
+
+    }, 
+    password: {
+        type: String,
+        required: true, 
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            //The .includes() returns a true the 'password' is included 
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password can not containt the  word "Password')
+            }
+        }
+
+    },
+    email: {
+      type: String, 
+      required: true,
+      trim: true, 
+      lowercase: true,
+      validate(value){
+          //We are going to use the validator library  and we are going to pass the value entered
+          if(!validator.isEmail(value)){
+
+  throw new Error ('Email is invalid')
+          }
+      }
+    }, 
+
+    age: {
+    type: Number,
+    default: 0, 
+    validate(value){
+        if(value < 0){
+            throw new Error('Age must be a positive number')
+        }
+    }
+
+    }
+} )
+
