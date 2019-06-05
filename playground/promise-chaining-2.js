8f4bd09cbd3729fec86f4a5030426b6493ca70cc
Aaron <aaron.botello.h@gmail.com>
2019-06-05 17:26:32 -0500

    Getting the task deletion going

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,12 @@
+require('../src/db/mongoose')
+
+ const Task = require('../src/models/task')
+
+ Task.findByIdAndDelete('5cf29bcbcdb0c52d3cd52048').then((task) => {
+     console.log(task)
+     return Task.countDocuments({ completed: false})
+ }).then((result) =>{
+     console.log(result)
+ }).catch((e) =>{
+     console.log()
+ })
