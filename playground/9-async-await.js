6e975d206299337a265bf80ca91cd50e4115d8bb
Aaron <aaron.botello.h@gmail.com>
2019-06-05 18:11:30 -0500

    using the amait functionality

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -1,10 +1,24 @@
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+
 
 //creating async functionality 
 const doWork = async () => {
+const sum = await add(1,99)
 
+    //throw new Error('Somenting went wrong')
+    return sum 
 }
 
 // console.log(doWork())
+
+
 
 //When the .then() its going to run when the promise is fullfield 
 doWork().then((result) =>{
