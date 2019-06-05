c0d4998e5990fb6f95022f03c57c506fc76d7c02
Aaron <aaron.botello.h@gmail.com>
2019-06-05 17:27:40 -0500

    findByIdAndDelete function used

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,11 +2,12 @@ require('../src/db/mongoose')
 
  const Task = require('../src/models/task')
 
+ //Getting a tag deletion using findByIdAndDelete()
  Task.findByIdAndDelete('5cf29bcbcdb0c52d3cd52048').then((task) => {
      console.log(task)
      return Task.countDocuments({ completed: false})
  }).then((result) =>{
      console.log(result)
  }).catch((e) =>{
-     console.log()
+     console.log(e)
  })
