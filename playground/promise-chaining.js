7557e7fc093ec4ee05c7a5cb0b861b7de676d22c
Aaron <aaron.botello.h@gmail.com>
2019-06-04 20:35:57 -0500

    Getting the id object to update

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,3 +2,7 @@ require('../src/db/mongoose')
 
 const User = require('../src/models/user')
 
+// 5cf2f2ece1cf6677b43a6cd5 
+
+//The second is an object 
+User.findByIdAndUpdate('5cf2f2ece1cf6677b43a6cd5', {}
