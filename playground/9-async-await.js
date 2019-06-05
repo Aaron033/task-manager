e1f23d9707ab4e58005eadfe03eb0889ef0e0ff8
Aaron <aaron.botello.h@gmail.com>
2019-06-05 18:03:26 -0500

    working with the async await

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -4,4 +4,12 @@ const doWork = async () => {
 
 }
 
-console.log(doWork())
+// console.log(doWork())
+
+//When the .then() its going to run when the promise is fullfield 
+doWork().then((result) =>{
+//I get access to the result 
+console.log('Result', result)
+}).catch((e) =>{ 
+    console.log('e', e)
+})
