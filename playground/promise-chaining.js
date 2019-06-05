f1c5692356972471dbd7032e01451de45111f476
Aaron <aaron.botello.h@gmail.com>
2019-06-04 20:37:51 -0500

    Setting up a new chance

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -5,4 +5,7 @@ const User = require('../src/models/user')
 // 5cf2f2ece1cf6677b43a6cd5 
 
 //The second is an object 
-User.findByIdAndUpdate('5cf2f2ece1cf6677b43a6cd5', {}
+User.findByIdAndUpdate('5cf2f2ece1cf6677b43a6cd5', { age: 21}).then((user) => {
+console.log(user)
+
+})
