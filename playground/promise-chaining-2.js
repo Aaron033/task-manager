93f9bdb009ef76b774634a3d90d6362cb30e8d80
Aaron <aaron.botello.h@gmail.com>
2019-06-06 20:12:17 -0500

    function working as it should

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,21 +2,21 @@ require('../src/db/mongoose')
 
  const Task = require('../src/models/task')
 
- //Getting a tag deletion using findByIdAndDelete()
- Task.findByIdAndDelete('5cf29bcbcdb0c52d3cd52048').then((task) => {
-     console.log(task)
-     return Task.countDocuments({ completed: false})
- }).then((result) =>{
-     console.log(result)
- }).catch((e) =>{
-     console.log(e)
- })
+//  //Getting a tag deletion using findByIdAndDelete()
+//  Task.findByIdAndDelete('5cf29bcbcdb0c52d3cd52048').then((task) => {
+//      console.log(task)
+//      return Task.countDocuments({ completed: false})
+//  }).then((result) =>{
+//      console.log(result)
+//  }).catch((e) =>{
+//      console.log(e)
+//  })
 
  const deleteTaskAndCount = async (id) => {
      const task = await Task.findByIdAndDelete(id)
-
+//We pass the id of the perimeter 
      const count = await Task.countDocuments({completed:false })
-
+ //Count docuemnts returns a promise 
      return count
  }
 
