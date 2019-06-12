d232bfb00980b6b423eed0f34d5aec08b047d8ae
Aaron <aaron.botello.h@gmail.com>
2019-06-11 20:37:49 -0500

    code is not updating

--- src/index.js
+++ src/index.js
@@ -181,6 +181,7 @@ app.patch('/tasks/:id', async (req, res) => {
         // second parameter the updates that we trying to apply on 
         const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true}) 
         
+        //It would return a error 
         if(!task){
             return res.status(404).send()
         }
