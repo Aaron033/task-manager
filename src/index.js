49aa806baddce144b5a961a5f475f6a9075702d5
Aaron <aaron.botello.h@gmail.com>
2019-06-19 20:49:44 -0500

    deleting users by id function created

--- src/index.js
+++ src/index.js
@@ -107,9 +107,12 @@ if(!isValidOperation) {
 app.delete('/users/:id', async (req, res) => {
     try {
   const user = await User.findByIdAndDelete(req.params.id)
-  if(!user)
+  if(!user){
+      return res.status(404).send()
+  }
+  res.send(user)
     }catch(e){
-
+res.status(500).send()
     }
 })
 
