40979a8bb050494b34d1bdebc02fc03210521b63
Aaron <aaron.botello.h@gmail.com>
2019-06-22 16:11:10 -0500

    creating an example router

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 
 
 const router = new express.Router()
+
+router.get('/test' , (req, res) => {
+    res.send('This is from my other router')
+})
 //********************************************************************************************* */
 
 
