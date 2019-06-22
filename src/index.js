8b4adbe599e24fff55a72fa8cff309ce7ef3dd96
Aaron <aaron.botello.h@gmail.com>
2019-06-22 16:26:05 -0500

    code not working, memory leak

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
 require('./db/mongoose')
+
 const app = express()
 //It is not going to grab anything from the file; it just going to ensure 
 //That mongoose connects to the data base
 //The code below is needed in order to develop the app in heroku 
 const port = process.env.PORT || 3000 
 app.use(express.json())
-
+app.use(userRouter)
 
 
 
