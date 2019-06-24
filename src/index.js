511b3fe5e190b2120dd87dfbbca0b117c0441132
Aaron <aaron.botello.h@gmail.com>
2019-06-24 16:35:32 -0500

    router connection added in the index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('./models/user')
 const Task = require('./models/task')
 const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 require('./db/mongoose')
 
 const app = express()
@@ -11,6 +12,8 @@ const app = express()
 const port = process.env.PORT || 3000 
 app.use(express.json())
 app.use(userRouter)
+app.use(taskRouter)
+
 
 
 
