cf68984840554aad644a7765dc174d17ae340653
Aaron <aaron.botello.h@gmail.com>
2019-06-25 18:05:49 -0500

    adding the schemma functionality

--- src/models/user.js
+++ src/models/user.js
@@ -3,54 +3,52 @@ const validator = require('validator')
 // Validator to check emails, credit cards, etc. 
 
 const userSchema = new mongoose.Schema(
-
-const User = mongoose.model('User', {
-    name: {
-    type: String,
-
-    trim: true,
-    require: true
-
-    }, 
-    password: {
+    {
+        name: {
         type: String,
-        required: true, 
+    
         trim: true,
-        minlength: 7,
-        validate(value) {
-            //The .includes() returns a true the 'password' is included 
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password can not containt the  word "Password"')
+        require: true
+    
+        }, 
+        password: {
+            type: String,
+            required: true, 
+            trim: true,
+            minlength: 7,
+            validate(value) {
+                //The .includes() returns a true the 'password' is included 
+                if(value.toLowerCase().includes('password')){
+                    throw new Error('Password can not containt the  word "Password"')
+                }
+            }
+    
+        },
+        email: {
+          type: String, 
+          required: true,
+          trim: true, 
+          lowercase: true,
+          validate(value){
+              //We are going to use the validator library  and we are going to pass the value entered
+              if(!validator.isEmail(value)){
+    
+      throw new Error ('Email is invalid')
+              }
+          }
+        }, 
+    
+        age: {
+        type: Number,
+        default: 0, 
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a positive number')
             }
         }
-
-    },
-    email: {
-      type: String, 
-      required: true,
-      trim: true, 
-      lowercase: true,
-      validate(value){
-          //We are going to use the validator library  and we are going to pass the value entered
-          if(!validator.isEmail(value)){
-
-  throw new Error ('Email is invalid')
-          }
-      }
-    }, 
-
-    age: {
-    type: Number,
-    default: 0, 
-    validate(value){
-        if(value < 0){
-            throw new Error('Age must be a positive number')
+    
         }
-    }
-
-    }
-} )
-)
+    } )
 
 
 
