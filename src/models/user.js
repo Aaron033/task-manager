15725942d509e6e4b9f2e081a0e3e9a3ffd1334c
Aaron <aaron.botello.h@gmail.com>
2019-06-26 21:08:43 -0500

    bcrypt library to documents

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 // Validator to check emails, credit cards, etc. 
 
 const userSchema = new mongoose.Schema(
