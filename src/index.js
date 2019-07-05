656f904ffb8da9545b67213cd059c919e1aea597
Aaron <aaron.botello.h@gmail.com>
2019-07-04 22:24:38 -0500

    creating a token

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ app.use(taskRouter)
 app.listen(port, () => { 
     console.log('Server is up on port ' + port)
 
+    const jwt = require('jsonwebtoken')
 const myFunction = async() =>{ 
+const token = jwt.sign({_id: 'abc123'}, ' ')
 
 }
 
