7b988974cee9bafec41dd65925475bf304a7792b
Aaron <aaron.botello.h@gmail.com>
2019-07-05 21:01:00 -0500

    given the expiretion to a token

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.listen(port, () => {
 
     const jwt = require('jsonwebtoken')
 const myFunction = async() =>{ 
-const token = jwt.sign({_id: 'abc123'}, 'thisismyaaron')
+const token = jwt.sign({_id: 'abc123'}, 'thisismyaaron', {expiresIn: ""})
 console.log(token)
 
 const data = jwt.verify(token, 'thisismyaaron')
