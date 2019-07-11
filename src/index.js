1846e0a930f7b0d1d684290fcd7adeaee2900160
Aaron <aaron.botello.h@gmail.com>
2019-07-10 20:31:38 -0500

    adding comments to the code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express()
 //It is not going to grab anything from the file; it just going to ensure that mongoose connects to the data base
 //The code below is needed in order to develop the app in heroku 
 const port = process.env.PORT || 3000 
+//It is either heroku or set up port 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
