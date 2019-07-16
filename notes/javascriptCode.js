85b027712e8bc24119c5124f5d102b5d5ee9a174
Aaron <aaron.botello.h@gmail.com>
2019-07-15 20:46:36 -0500

    adding more properties and functions

--- notes/javascriptCode.js
+++ notes/javascriptCode.js
@@ -157,6 +157,11 @@ const another = new Circle(1);
 // ###############################################################################
 function Circle(radius){
     this.radius = radius; 
+
+    this.defaultLocation = { x:0, y: 0} 
+    this.computeOptimumLocation =  function() {
+
+    }
     this.draw = function(){ 
         console.log('draw')
     }
