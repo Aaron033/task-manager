7412d4b1aaf22737c7fbab3e49b48f948924c5e2
Aaron <aaron.botello.h@gmail.com>
2019-07-16 20:51:05 -0500

    usual  information added

--- notes/testingDoc.js
+++ notes/testingDoc.js
@@ -9,7 +9,7 @@ function automobile(type){
 
 const car = new automobile('chevy')
 console.log(car)
-
+//This code gives you an array of keys of a function passed as parameter 
 const keys = Object.keys(car)
 
 console.log(keys)
