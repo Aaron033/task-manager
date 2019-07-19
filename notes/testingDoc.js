a2fc4748ada9e18fd00505720cf4246a3fa5d289
Aaron <aaron.botello.h@gmail.com>
2019-07-18 20:34:47 -0500

    code added

--- notes/testingDoc.js
+++ notes/testingDoc.js
@@ -1,15 +1,15 @@
-// function automobile(type){
-//     this.type = type, 
+function automobile(type){
+    this.type = type, 
 
-//     this.drive = function() {
-//         console.log(drives)
-//     }
-//     console.log("Hello")
-// }
+    this.drive = function() {
+        console.log(drives)
+    }
+    console.log("Hello")
+}
 
-// const car = new automobile('chevy')
-// console.log(car)
-// //This code gives you an array of keys of a function passed as parameter 
-// const keys = Object.keys(car)
+const car = new automobile('chevy')
+console.log(car)
+//This code gives you an array of keys of a function passed as parameter 
+const keys = Object.keys(car)
 
-// console.log(keys)
+console.log(keys)
