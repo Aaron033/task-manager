1665168c8ab0354026c255e13a3c9f4ca1ad6a73
Aaron <aaron.botello.h@gmail.com>
2019-07-19 15:46:40 -0500

    creating a Stopwatch

--- notes/testingDoc.js
+++ notes/testingDoc.js
@@ -13,3 +13,23 @@
 // const keys = Object.keys(car)
 
 // console.log(keys)
+
+function Stopwatch() { 
+    let startTime, endTime, running, duration= 0; 
+
+    this.start = function() { 
+
+    }
+
+    this.stop = function() { 
+
+    }
+
+    this.reset = function() { 
+
+    }
+
+    Object.defineProperty(this, 'duration', { 
+        
+    })
+}
