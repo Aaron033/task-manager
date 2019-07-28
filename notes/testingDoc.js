c5fd1e3b5a2d5ba9ddd32392db89476e65a949ac
Aaron <aaron.botello.h@gmail.com>
2019-07-27 21:45:39 -0500

    code fixed

--- notes/testingDoc.js
+++ notes/testingDoc.js
@@ -1,9 +1,12 @@
-let one = document.querySelector(".on");
+// let one = document.querySelector(".on");
 
-function change(a){
-    a.preventDefault(); 
-    one.innerHTML = "Hell"
-}
+// function change(a){
+//     a.preventDefault(); 
+//     one.innerHTML = "Hell"
+// }
 
-one.onclick = change;
+// one.onclick = change;
 
+let add = document.querySelector(".ad").addEventListener("click", function(){
+    console.log("add")
+})
