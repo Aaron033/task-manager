4832798458ea7c6ad49f74046c2ca4e6d12ffc40
Aaron <aaron.botello.h@gmail.com>
2019-07-29 20:34:55 -0500

    getting the data equals

--- notes/calc.js
+++ notes/calc.js
@@ -1,2 +1,4 @@
 const numberButtons = document.querySelectorAll('data-number'); 
-const operationButtons = document.querySelectorAll('[data-operation]')
+const operationButtons = document.querySelectorAll('[data-operation]'); 
+
+const equalsButton = document.querySelector('[data-equals]')
