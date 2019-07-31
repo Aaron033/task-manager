8d0945df6ccd71843973189c15990c641c8e356a
Aaron <aaron.botello.h@gmail.com>
2019-07-30 21:01:07 -0500

    creating a instance of a Calculator

--- notes/calc.js
+++ notes/calc.js
@@ -38,3 +38,6 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const  currentOperandTextElement= document.querySelector('[data-current-operand]');
+
+
+const calculator = new Calculator()
