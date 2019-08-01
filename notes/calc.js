879ab3a9f11c3546e3ede0e4268addd5262b5696
Aaron <aaron.botello.h@gmail.com>
2019-07-31 19:42:05 -0500

    looping throught code

--- notes/calc.js
+++ notes/calc.js
@@ -47,5 +47,9 @@ const  currentOperandTextElement= document.querySelector('[data-current-operand]
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
+    button.addEventListener("click", () =>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
 
 })
